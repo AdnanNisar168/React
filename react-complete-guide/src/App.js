0dc29fc00158ddcba4c23837136b11333da09216
adnan <adnan.nisar@quicktijarat.com>
2023-08-02 21:44:47 +0500

    working on jsx

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,8 +1,12 @@
+// import ExpenseItem from "./Components/ExpenseItem";
+import ExpenseItem from "./Components/ExpenseItem.js";
 
 function App() {
   return (
     <div className="App">
       <h2>Let's Get Started</h2>
+      <p>this also visible</p>
+      <ExpenseItem/>
     </div>
   );
 }
