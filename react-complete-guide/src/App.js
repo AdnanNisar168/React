809f8c678d92e5b184d3ad484cb0145a5eddca66
adnan <adnan.nisar@quicktijarat.com>
2023-08-12 11:25:34 +0500

    learning react

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,51 @@
 // import ExpenseItem from "./Components/ExpenseItem";
-import ExpenseItem from "./Components/ExpenseItem.js";
+import Expenses from "./Components/Expenses/Expenses";
+// is used in old react apps
+import React from 'react';
 
-function App() {
+const App = ()=> {
+
+  const expenses = [
+    {
+      title: 'Toilet Paper',
+      amount: '94.27',
+      date: new Date(2023, 7, 3)
+    },
+    {
+      title: 'New TV',
+      amount: '794.67',
+      date: new Date(2023, 8, 1)
+    },
+    {
+      title: 'Car Insurance',
+      amount: '294.67',
+      date: new Date(2023, 8, 3)
+    },
+    {
+      title: 'New Desk ( wooden )',
+      amount: '50',
+      date: new Date(2023, 4, 5)
+    },
+  ];
+
+  // without jsx
+          
+  // return React.createElement(
+  //   'div',
+  //    {},                //name of tag, attribute, value
+  //     React.createElement('h2', {}, "Let's Get Started"),
+  //     React.createElement(Expenses, {items: expenses})
+  //     );
+  // without jsx
+
+
   return (
     <div className="App">
       <h2>Let's Get Started</h2>
-      <p>this also visible</p>
-      <ExpenseItem/>
+      
+      <Expenses items={expenses} />
+
+
     </div>
   );
 }
---
+++ react-complete-guide/src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,42 @@
+import Card from '../UI/Card';
+import ExpenseDate from './ExpenseDate';
+
+// importing css 
+import './ExpenseItem.css';
+
+
+function ExpenseItem(props) {
+    // javaScript code
+    const expenseDate = new Date(2023, 8, 3);
+    const expenseTile = 'Car Insurance';
+    const expenseAmount = 294.67;
+    let title = props.title
+    const clickHandler = ()=>{
+        title = 'Updated'
+        console.log(title);
+    }
+    
+    return (
+        // <div className="expense-item">
+        // <div>{ expenseDate.toISOString() }</div>
+        // <div className="expense-item__description">
+        //     <h2>{ expenseTile }</h2>
+        //     <div className="item__price">${ expenseAmount }</div>
+        // </div>
+        // </div>
+        <Card className="expense-item">
+            <ExpenseDate date={props.date} />
+            
+        <div className="expense-item__description">
+            <h2>{ title }</h2>
+            <div className="expense-item__price">
+                ${ props.amount }
+            </div>
+        </div>
+        {/* <button onClick = {()=>{console.log('clicked!')}}>Change Title</button> */}
+        <button onClick ={clickHandler} >Change Title</button>
+        </Card>
+    );
+}
+
+export default ExpenseItem; 
